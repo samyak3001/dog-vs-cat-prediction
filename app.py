6da2b485044ec2c2d38e04cee1d5439e7d8a2087
samyak3001 <m.s.samyak@gmail.com>
2026-04-12 01:21:00 +0530

    Switch from tflite_runtime to TensorFlow for model loading

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import numpy as np
 from PIL import Image
 import urllib.request
 import os
-import tflite_runtime.interpreter as tflite
+import tensorflow as tf
 
-# Download model WITHOUT gdown
+# Download model
 MODEL_URL = "https://drive.google.com/uc?export=download&id=1nn78IVOV55kUdI96nUjNjqrfu-BP---c"
 MODEL_PATH = "model.tflite"
 
 if not os.path.exists(MODEL_PATH):
     urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
 
-# Load model
-interpreter = tflite.Interpreter(model_path=MODEL_PATH)
+# Load model using TensorFlow Lite
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
