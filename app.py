71315bcaf3b73317d977513680c82218970596e4
samyak3001 <m.s.samyak@gmail.com>
2026-04-12 01:15:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import gdown
+import urllib.request
+import os
 import tflite_runtime.interpreter as tflite
 
-# Download model from Google Drive
-url = "https://drive.google.com/uc?id=1nn78IVOV55kUdI96nUjNjqrfu-BP---c"
-gdown.download(url, "model.tflite", quiet=False)
+# Download model WITHOUT gdown
+MODEL_URL = "https://drive.google.com/uc?export=download&id=1nn78IVOV55kUdI96nUjNjqrfu-BP---c"
+MODEL_PATH = "model.tflite"
+
+if not os.path.exists(MODEL_PATH):
+    urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
 
 # Load model
-interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter = tflite.Interpreter(model_path=MODEL_PATH)
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # UI
-st.set_page_config(page_title="Animal Classifier", page_icon="🐾")
-
 st.title("🐶🐱 Cat vs Dog Classifier")
-st.write("Upload an image to classify")
 
 uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png"])
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    st.image(img)
 
     img = img.resize((160, 160))
     img = np.array(img, dtype=np.float32) / 255.0
