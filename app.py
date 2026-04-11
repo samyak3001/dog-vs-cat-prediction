b63d3ef372e18ae442951f69408ffa1fb9c513c9
samyak3001 <m.s.samyak@gmail.com>
2026-04-12 01:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-from keras.models import load_model
 import gdown
+import tflite_runtime.interpreter as tflite
 
-# Download model
-url = "https://drive.google.com/uc?id=15HYsAWvbom5uwc8F3WtIssfgfC4M_FjA"
-gdown.download(url, "model.keras", quiet=False)
+# Download model from Google Drive
+url = "https://drive.google.com/uc?id=1nn78IVOV55kUdI96nUjNjqrfu-BP---c"
+gdown.download(url, "model.tflite", quiet=False)
 
 # Load model
-model = load_model("model.keras")
+interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 # UI
 st.set_page_config(page_title="Animal Classifier", page_icon="🐾")
@@ -24,14 +28,14 @@ if uploaded_file is not None:
     st.image(img, caption="Uploaded Image", use_column_width=True)
 
     img = img.resize((160, 160))
-    img = np.array(img) / 255.0
+    img = np.array(img, dtype=np.float32) / 255.0
     img = np.expand_dims(img, axis=0)
 
-    prediction = model.predict(img)[0][0]
+    interpreter.set_tensor(input_details[0]['index'], img)
+    interpreter.invoke()
+    prediction = interpreter.get_tensor(output_details[0]['index'])[0][0]
 
     if prediction > 0.5:
         st.success("🐶 Dog")
-        st.write(f"Confidence: {prediction:.2f}")
     else:
         st.success("🐱 Cat")
-        st.write(f"Confidence: {1 - prediction:.2f}")
